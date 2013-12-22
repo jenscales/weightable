870ff7b4043bd279b752be605829709b464b1fe2
jenscales <jen_scales@yahoo.com>
2013-12-21 19:48:07 -0600

    Add index view and some css for the page with banner pic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Weightable::Application.routes.draw do
 
   devise_for :users
-  root :to => "home#index"
+  root :to => "User#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
