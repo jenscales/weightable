e4fd27a34cc79902801862493387c5cfe831d42a
jenscales <jen_scales@yahoo.com>
2013-12-21 22:10:35 -0600

    add user resources to routes, update index page for some css and foundation, linked home page to sign-up and sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Weightable::Application.routes.draw do
 
   devise_for :users
-  root :to => "User#index"
+  root :to => "users#index"
+
+  resources :users do
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
