c243c401f87ded25493e34000061b230acd65a16
jenscales <jen_scales@yahoo.com>
2013-12-21 19:00:22 -0600

    create weightable project and generate User model and Friend model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weightable::Application.config.secret_key_base = '3efc5d661cf17f69ba6458f6ccef2e4a90a3b3edd6de9b6961f3984a84c46d456d6717c1416c3780c5944e50d1642405224ded8c42802da6f804a9785fdad745'
