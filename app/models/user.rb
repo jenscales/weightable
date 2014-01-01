c46f9b4bd3d8576d01165252d822e3717675e002
jenscales <jen_scales@yahoo.com>
2014-01-01 00:06:01 -0600

    updated user show and model for carrier wave and image rendering properly on page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :user_image, ImageUploader       
+
   def new
     @user = User.new
   end
